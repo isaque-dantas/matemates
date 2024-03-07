7431d0b3599ed8561a14de603d3d8785360aa944
isaque-dantas <vicisaque413@gmail.com>
2024-03-07 17:58:06 -0300

    start of templates adaptation

--- app/controllers/default.py
+++ app/controllers/default.py
@@ -6,6 +6,6 @@ blueprint = Blueprint('default', __name__)
 @blueprint.route('/')
 @blueprint.route('/index')
 def index():
-    return render_template('index.html')
+    return render_template('usuario.html')
 
 
