3837e802b6cf3fec2e980eae0b97f6b01db5989f
isaque-dantas <vicisaque413@gmail.com>
2024-03-08 16:54:16 -0300

    database snapshot

---
+++ db_snapshot_08_03.sql
@@ -0,0 +1,199 @@
+-- MySQL dump 10.13  Distrib 8.0.36, for Linux (x86_64)
+--
+-- Host: localhost    Database: matemates_db
+-- ------------------------------------------------------
+-- Server version	8.0.36
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `definition`
+--
+
+DROP TABLE IF EXISTS `definition`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `definition` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `content` varchar(256) NOT NULL,
+  `related_math_area` enum('álgebra','cálculo','geometria','estatística e probabilidade','trigonometria','teoria dos números','matemática discreta') DEFAULT NULL,
+  `order` int NOT NULL,
+  `term_id` int DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `term_id` (`term_id`),
+  CONSTRAINT `definition_ibfk_1` FOREIGN KEY (`term_id`) REFERENCES `term` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `definition`
+--
+
+LOCK TABLES `definition` WRITE;
+/*!40000 ALTER TABLE `definition` DISABLE KEYS */;
+INSERT INTO `definition` VALUES (1,'Nome da primeira letra do alfabeto grego α, A.','geometria',1,1),(2,'Aquilo que dá origem a; princípio: a musa é o alfa do pintor','trigonometria',2,1),(3,'Estrela principal de uma constelação','álgebra',3,1);
+/*!40000 ALTER TABLE `definition` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `image`
+--
+
+DROP TABLE IF EXISTS `image`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `image` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `path` varchar(256) NOT NULL,
+  `caption` varchar(256) DEFAULT NULL,
+  `term_id` int DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `term_id` (`term_id`),
+  CONSTRAINT `image_ibfk_1` FOREIGN KEY (`term_id`) REFERENCES `term` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `image`
+--
+
+LOCK TABLES `image` WRITE;
+/*!40000 ALTER TABLE `image` DISABLE KEYS */;
+INSERT INTO `image` VALUES (1,'alfa.png',NULL,1);
+/*!40000 ALTER TABLE `image` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `question`
+--
+
+DROP TABLE IF EXISTS `question`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `question` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `statement` varchar(256) NOT NULL,
+  `answer` varchar(256) NOT NULL,
+  `explanation` varchar(256) DEFAULT NULL,
+  `order` int NOT NULL,
+  `term_id` int DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `term_id` (`term_id`),
+  CONSTRAINT `question_ibfk_1` FOREIGN KEY (`term_id`) REFERENCES `term` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `question`
+--
+
+LOCK TABLES `question` WRITE;
+/*!40000 ALTER TABLE `question` DISABLE KEYS */;
+INSERT INTO `question` VALUES (1,'Qual o seno de 30°?','1/2',NULL,1,1),(2,'Quantas maçãs moisés botou na arca?','0','Não foi Moisés, e nem tinha maçã no meio da história.',4,1),(3,'Aproximadamente qual a aceleração da gravidade na superfície da Terra?','10 m/s²','Isso é um dado empírico. Em outras palavras, chega-se a ele a partir de medições e experimentos.',3,1),(4,'Linux > Windows. Porque?','Porque Linux não é da Microsoft. Ela deveria ser banida, e deixar todo o código dela para o ambiente coletivo.','O amor venceu. Esse é o motivo.',2,1);
+/*!40000 ALTER TABLE `question` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `syllable`
+--
+
+DROP TABLE IF EXISTS `syllable`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `syllable` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `content` varchar(6) NOT NULL,
+  `order` int NOT NULL,
+  `term_id` int DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `term_id` (`term_id`),
+  CONSTRAINT `syllable_ibfk_1` FOREIGN KEY (`term_id`) REFERENCES `term` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `syllable`
+--
+
+LOCK TABLES `syllable` WRITE;
+/*!40000 ALTER TABLE `syllable` DISABLE KEYS */;
+INSERT INTO `syllable` VALUES (1,'al',1,1),(2,'fa',2,1);
+/*!40000 ALTER TABLE `syllable` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `term`
+--
+
+DROP TABLE IF EXISTS `term`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `term` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `gender` enum('M','F') NOT NULL,
+  `content` varchar(256) NOT NULL,
+  `grammatical_category` enum('substantivo','verbo','adjetivo','numeral') NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `term`
+--
+
+LOCK TABLES `term` WRITE;
+/*!40000 ALTER TABLE `term` DISABLE KEYS */;
+INSERT INTO `term` VALUES (1,'F','alfa','substantivo');
+/*!40000 ALTER TABLE `term` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `user` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `first_name` varchar(32) NOT NULL,
+  `last_name` varchar(32) NOT NULL,
+  `email` varchar(128) NOT NULL,
+  `password_hash` varchar(256) NOT NULL,
+  `username` varchar(24) NOT NULL,
+  `phone_number` varchar(32) DEFAULT NULL,
+  `birth_date` date NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `email` (`email`),
+  UNIQUE KEY `username` (`username`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user`
+--
+
+LOCK TABLES `user` WRITE;
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2024-03-08 16:51:50
