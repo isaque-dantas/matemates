fb5710d54e23c8281f17d9dc6b0cf697135d8967
isaque-dantas <vicisaque413@gmail.com>
2024-03-23 16:23:51 -0300

    add search-box funcionality

---
+++ app/static/js/handle-search-box-enter-event.js
@@ -0,0 +1,21 @@
+const searchInputs = document.querySelectorAll('.search-box > input.search-txt')
+
+// document.addEventListener(
+//     'keypress',
+//     (event) => {
+//         console.log(event.key)
+//     }
+// )
+
+searchInputs.forEach(
+    (searchInput) => {
+        searchInput.addEventListener(
+            'keypress',
+            (event) => {
+                if (event.key === 'Enter') {
+                    location.href=`/search/${searchInput.value}`
+                }
+            }
+        )
+    }
+)
