a939d2a499ce934a34fb1279f1012c5beb335cce
Davi <davibezerra123457@gmail.com>
2024-03-26 18:33:39 -0300

    bolhas agora geradas no JavaScript

---
+++ app/static/js/bolhas-hotfix.js
@@ -0,0 +1,9 @@
+const fundoBolhas = document.querySelector(".fundo-bolhas");
+
+window.onload = function () {
+  for (i = 0; i < 100; i++) {
+    let newSpan = document.createElement("span");
+    newSpan.style = `--i: ${Math.random() * (30-5) + 5}`;
+    fundoBolhas.appendChild(newSpan);
+  }
+};
