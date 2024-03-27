e8c51e27592e0c425cb8f5165797472bacb5d7eb
Davi <davibezerra123457@gmail.com>
2024-03-26 23:57:39 -0300

    aumentei o número de bolhas

--- app/static/js/bolhas-hotfix.js
+++ app/static/js/bolhas-hotfix.js
@@ -1,7 +1,7 @@
 const fundoBolhas = document.querySelector(".fundo-bolhas");
 
 window.onload = function () {
-  for (i = 0; i < 100; i++) {
+  for (i = 0; i < 140; i++) {
     let newSpan = document.createElement("span");
     newSpan.style = `--i: ${Math.random() * (30-5) + 5}`;
     fundoBolhas.appendChild(newSpan);
