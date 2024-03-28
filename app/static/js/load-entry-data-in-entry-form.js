4d21d15081d7655664f13917a2cc5d26cbfa4506
isaque-dantas <vicisaque413@gmail.com>
2024-03-27 22:57:58 -0300

    load entry data in entry form when editing

--- app/static/js/load-entry-data-in-entry-form.js
+++ app/static/js/load-entry-data-in-entry-form.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
     if (itsEditingPage()) {
         const entryDataPromise = getEntryData()
-
+        updateDocumentFormData(entryDataPromise)
     }
 })
 
@@ -10,18 +10,59 @@ function itsEditingPage() {
 }
 
 async function getEntryData() {
-    const response = await fetch(`/entry_data/${getEntryContent()}`)
-    const json = await response.json()
-    console.log(json)
-    return json
+    const response = await fetch(`/entry_data/${getEntryID()}`)
+    return await response.json()
 }
 
 async function updateDocumentFormData(entryDataPromise) {
     const entryData = await entryDataPromise
+    console.log(entryData)
+    console.log()
 
+    const fixedProperties = entryData['fixed_properties']
+    updateDocumentFormFixedProperties(fixedProperties)
+
+    const nProperties = entryData['n_properties']
+    updateDocumentFormNProperties(nProperties)
 }
 
-function getEntryContent() {
+function updateDocumentFormFixedProperties(fixedProperties) {
+    for (const fixedProperty in fixedProperties) {
+        const formField = document.querySelector(`[name="${fixedProperty}"]`)
+
+        setPropertyToFormField(fixedProperties[fixedProperty], formField)
+    }
+}
+
+function updateDocumentFormNProperties(nProperties) {
+    for (const nProperty in nProperties) {
+        let formField = document.querySelector(`[name="${nProperty}"]`)
+
+        if (formField === null) {
+            const fieldGroupsID = `${getEntityNameFromNProperty(nProperty)}-field-groups`
+            const fieldGroups = document.querySelector(`#${fieldGroupsID}`)
+            addFieldGroup(fieldGroups)
+            formField = document.querySelector(`[name="${nProperty}"]`)
+        }
+
+        setPropertyToFormField(nProperties[nProperty], formField)
+    }
+}
+
+function setPropertyToFormField(propertyValue, formField) {
+    if (formField.tagName === 'SELECT') {
+        const optionTarget = formField.querySelector(`option[value="${propertyValue}"]`)
+        optionTarget.selected = true
+    } else if (formField.tagName === 'INPUT') {
+        formField.value = propertyValue
+    }
+}
+
+function getEntityNameFromNProperty(nProperty) {
+    return nProperty.split('_')[0]
+}
+
+function getEntryID() {
     return location.href.split('/')[4]
 }
 
