be0f0ed874385d24075704e2da623af9310d5ec1
isaque-dantas <vicisaque413@gmail.com>
2024-04-03 22:19:43 -0300

    add validation for just getting image files

--- app/static/js/change_image.js
+++ app/static/js/change_image.js
@@ -3,20 +3,28 @@ const previewImage = document.querySelector(".preview-image");
 
 // Listen for changes in the file input
 imageInput.addEventListener("change", function (e) {
-  const file = e.target.files[0];
+    const file = e.target.files[0];
 
-  if (file) {
-    // Create the FileReader object
-    const reader = new FileReader();
+    if (file) {
+        // Create the FileReader object
+        const reader = new FileReader();
 
-    // Set up the onload event handler
-    reader.onload = function (e) {
-      // Set the source of the image element to the data URL
-      previewImage.src = e.target.result;
-      previewImage.style.padding = 0;
-    };
+        // Set up the onload event handler
+        reader.onload = function (e) {
+            // Set the source of the image element to the data URL
+            const image = e.target.result
+            const imageMatch = image.match('^data:image/.*')
 
-    // Read the contents of the file as a data URL
-    reader.readAsDataURL(file);
-  }
+            if (imageMatch) {
+                previewImage.src = e.target.result;
+                previewImage.style.padding = 0;
+
+            } else {
+                alert('Escolha apenas imagens')
+            }
+        };
+
+        // Read the contents of the file as a data URL
+        reader.readAsDataURL(file);
+    }
 });
