750061b548a06fd7b63c9538921be546c42b5943
isaque-dantas <vicisaque413@gmail.com>
2024-04-05 22:52:40 -0300

    add code for receive and send invite to be admin

--- app/controllers/dashboard.py
+++ app/controllers/dashboard.py
@@ -1,5 +1,5 @@
-from flask import render_template, Blueprint, redirect, url_for, flash
-from flask_login import AnonymousUserMixin, login_required, login_user, logout_user, current_user
+from flask import render_template, Blueprint
+from flask_login import login_required, current_user
 
 from app.models.tables import KnowledgeArea
 
@@ -9,4 +9,5 @@ dashboard_blueprint = Blueprint('dashboard', __name__)
 @dashboard_blueprint.route('/dashboard')
 @login_required
 def index():
-    return render_template("dashboard-index.html", knowledge_areas=KnowledgeArea.get_all(), list=list)
+    return render_template("dashboard-index.html", knowledge_areas=KnowledgeArea.get_all(), list=list,
+                           user=current_user)
