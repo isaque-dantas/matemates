3a41a9eef1227bf71febd60ab8539d63c241bd36
isaque-dantas <vicisaque413@gmail.com>
2024-04-08 17:20:27 -0300

    change way for getting FormData

--- app/static/js/fetch-edit-password-form.js
+++ app/static/js/fetch-edit-password-form.js
@@ -1,25 +1,49 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const form = document.querySelector('#edit-password-form')
-    form.addEventListener('submit', () => {
-        const formData = getFormDataFromForm(form)
-        sendFormData(formData, form.action)
+    const submitButton = document.querySelector('#edit-password-form-submit')
+    const urlPromise = getEditPasswordURLPromise()
+
+    submitButton.addEventListener('click', () => {
+        const formData = getFormData()
+        sendFormData(formData, urlPromise)
     })
 })
 
-function getFormDataFromForm(form) {
-    return new FormData(form)
+function getFormData() {
+    const formData = new FormData()
+    const inputs = document.querySelectorAll('input.edit-password-form-input')
+    inputs.forEach((input) => {
+        formData.append(input.name, input.value)
+    })
+
+    return formData
 }
 
-async function sendFormData(formData, url) {
+async function sendFormData(formData, urlPromise) {
+    const url = await urlPromise
     const response = await fetch(url, {
         method: 'POST',
-        headers: {
-            "Content-Type": "multipart/form-data",
-        },
         body: formData
     })
 
     if (!response.ok) {
         console.error(response.statusText)
     }
+
+    updateFeedbackMessage(await response.json())
+}
+
+async function getEditPasswordURLPromise() {
+    const username = await getCurrentUserUsernamePromise()
+    return `/edit_password/${username}`
+}
+
+async function getCurrentUserUsernamePromise() {
+    const response = await fetch('/user_data/')
+
+    if (!response.ok) {
+        console.error(response.statusText)
+    }
+
+    const jsonResponse = await response.json()
+    return jsonResponse['username']
 }
