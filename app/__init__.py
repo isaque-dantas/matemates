26458cc83ef40d0694bef9843c70bca374163b05
isaque-dantas <vicisaque413@gmail.com>
2024-04-11 13:15:27 -0300

    change redirect login message

--- app/__init__.py
+++ app/__init__.py
@@ -21,7 +21,7 @@ from app.models.tables import User
 login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = 'user.login'
-
+login_manager.login_message = 'Por favor, faça login para acessar essa página.'
 
 @login_manager.user_loader
 def load_user(user_id):
