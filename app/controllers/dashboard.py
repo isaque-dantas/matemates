732a85d6d3da9f6c52ce64f877c79921295ce292
isaque-dantas <vicisaque413@gmail.com>
2024-04-11 14:23:59 -0300

    remove list from render_template

--- app/controllers/dashboard.py
+++ app/controllers/dashboard.py
@@ -3,12 +3,13 @@ from flask_login import login_required, current_user
 
 from app.models.tables import KnowledgeArea
 
-from app.controllers.user import is_user_logged_in
+from app.controllers.user import is_user_logged_in, is_user_admin
 
 dashboard_blueprint = Blueprint('dashboard', __name__)
 
 
 @dashboard_blueprint.route('/dashboard')
 def index():
-    return render_template("dashboard-index.html", knowledge_areas=KnowledgeArea.get_all(), list=list,
-                           user=current_user, is_current_user_logged_in=is_user_logged_in(current_user))
+    return render_template("dashboard-index.html", knowledge_areas=KnowledgeArea.get_all(),
+                           user=current_user, is_current_user_logged_in=is_user_logged_in(current_user),
+                           is_user_admin=is_user_admin(current_user))
